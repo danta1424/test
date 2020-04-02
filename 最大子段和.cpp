534bbef03a389494e890b1f2737de983ae3ccc36
danta1424 <982518359@qq.com>
2020-04-02 12:11:59 +0800

    maxsum

---
+++ 最大子段和.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int max_sum(int* arr, int n) {
+	int pre = 0, max1 = 0;
+	for (int i = 1; i <= n; ++i) {
+		max1 = max(max1, pre = pre > 0 ? pre + arr[i] : arr[i]);
+	}
+	return max1;
+}
+
+int main() {
+	int n, a[100];
+	while (cin >> n) {
+		for (int m = 1; m <= n; m++)
+			cin >> a[m];
+		cout << max_sum(a,n) << endl;
+		cout << '\n';
+	}
+	return 0;
+}
